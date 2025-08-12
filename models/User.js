95b5ff1bd17a52202ed064e1788be4fd3b637dda
sidhutaran07 <taranpreets194@gmail.com>
2025-08-12 22:00:43 +0530

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,37 @@
+// models/User.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  quizzesTaken: [
+    {
+      quizId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Quiz',
+      },
+      score: {
+        type: Number,
+        required: true,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+});
+
+module.exports = mongoose.model('User', userSchema);
